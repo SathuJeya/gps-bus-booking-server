0a808f9e118f75481f803438f4924add119826da
Sathurya <66987255+SathuJeya@users.noreply.github.com>
2020-07-01 00:03:48 +0530

    update db config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    HOST: "localhost",
-    PORT: 27017,
+    HOST: "heroku_h5dwr9c2:ilpup8jaceddopc3a80uecdc13@ds245677.mlab.com",
+    PORT: 45677,
     DB: "heroku_h5dwr9c2"
 };
